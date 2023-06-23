a8bb7e4b0dfc9ee1460fa52d63a22cb3e96fbc05
Will Gallagher <39381406+Willlum@users.noreply.github.com>
2023-06-23 17:08:43 -0400

    added serial controls to stepper

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,52 @@
 #include <Arduino.h>
 #include <Servo.h> // Disabled definition of timer
+#include <SerialCommand.h> // change to serial port 3 
 //#include <TMC2209.h> // Uses softserial which restricts cpu time. Should only use softserial in setup
 
-#define DEBUG
+//#define DEBUG
+//#define DEBUG_SERIAL
+
 const uint16_t MIN_SPEED = 1040;
 const uint16_t MAX_SPEED =  2000;
-
 const uint8_t FAN = 9;
 const uint8_t MOTOR = 11;
 const uint8_t POT = 58;
-
 const uint8_t RX_PIN = 63;
 const uint8_t TX_PIN = 40;
 const uint8_t ENABLE_PIN = 38;
 const uint8_t STEP_PIN = 54;
 const uint8_t DIR_PIN = 55;
-
+const uint8_t TELEMETRY_FRAME_SIZE = 10;
+static uint8_t telemetryBuffer[TELEMETRY_FRAME_SIZE] = { 0, };
+uint8_t bufferPosition = 0;
+float eRevPerMin = 0;
 //SoftwareSerial soft_serial(RX_PIN, TX_PIN);
 //TMC2209 stepper_driver;
+Servo esc;
 const int32_t MAX_STEP_VELOCITY = 210000;
 const int32_t STOP_VELOCITY = 0;
 const int RUN_DURATION = 2000;
 const int STOP_DURATION = 1000;
 const int RUN_COUNT = 4;
-Servo esc;
 
-volatile uint32_t stepSpeed;
-volatile uint32_t stepCount = 0;
+volatile uint32_t stepSpeed = 50;
+volatile uint32_t curStepCount = 0;
+volatile uint32_t targetStepCount = 0;
  /*
   System clock 16 Mhz and Prescalar 8;
   Timer 1 speed = 16Mhz/8 = 2 MHz    
   Pulse time = 1/2 MHz  =  500ns
-  Count up to = 5us / 500ns = 10 (so this is the value the OCR register should have)
+  Count up to = 5us / 500ns = 10
 */  
 volatile uint32_t reload = 10;
+SerialCommand cmd;
 
-#define TELEMETRY_FRAME_SIZE 10
-static uint8_t telemetryBuffer[TELEMETRY_FRAME_SIZE] = { 0, };
-uint8_t bufferPosition = 0;
-float eRevPerMin = 0;
-uint8_t temperature = 0;
-
-ISR(TIMER3_COMPA_vect){
-  stepCount++;
-  TCNT3 = 0;  
-  OCR3A = reload * stepSpeed;
-  if(stepSpeed < 4990){
-    digitalWrite(STEP_PIN, HIGH);
-    digitalWrite(STEP_PIN, LOW);
-  }
-  if(stepCount == 100){
-    //digitalWrite(ENABLE_PIN, !digitalRead(ENABLE_PIN));
-    stepCount = 0;
-  }
-} //Timer 3 10us counter
 
 void setup() {
   #ifdef DEBUG
   Serial.begin(115200);
   #endif
+  Serial.begin(9600);
   Serial2.begin(115200);
   digitalWrite(FAN, HIGH);
   esc.attach(MOTOR, MIN_SPEED, MAX_SPEED);
@@ -68,7 +56,7 @@ void setup() {
 
   pinMode(STEP_PIN, OUTPUT);
   pinMode(ENABLE_PIN, OUTPUT);
-  digitalWrite(ENABLE_PIN, LOW);
+  digitalWrite(ENABLE_PIN, HIGH);
   pinMode(DIR_PIN, OUTPUT);
   digitalWrite(DIR_PIN, LOW);
   // stepper_driver.setup(soft_serial);
@@ -77,6 +65,13 @@ void setup() {
   // stepper_driver.enableCoolStep();
   // stepper_driver.moveAtVelocity(MAX_STEP_VELOCITY);
   // //stepper_driver.enable();
+
+  void unknownCommand(void);
+  void setMotorRPM(void);
+  void moveStepper(void);
+  cmd.addCommand("M1", setMotorRPM);
+  cmd.addCommand("S1", moveStepper);
+  cmd.addDefaultHandler(unknownCommand);
 
   //Timer 3 configuration
   noInterrupts();
@@ -89,7 +84,7 @@ void setup() {
 }
 
 void loop() {
-  while(Serial2.available()){
+  while(Serial2.available()){ //use rx interrupt handler
     telemetryBuffer[bufferPosition++] = Serial2.read();
 
     if(bufferPosition == TELEMETRY_FRAME_SIZE){
@@ -119,16 +114,63 @@ void loop() {
     }
   }
 
+  cmd.readSerial();
+  
   uint16_t val = analogRead(POT);
   uint16_t motorspeed = map(val, 0, 1023, MIN_SPEED, MAX_SPEED);
   analogWrite(FAN, map(val, 0, 1023, 0, 255)); // timer 2 is used for pwm
   
   //figure out best way to drive stepper. Currently ideling at 75mA
-  stepSpeed = map(val, 0, 1023, 5000, 50);
+  //stepSpeed = map(val, 0, 1023, 5000, 50);
+  
   
   esc.writeMicroseconds(motorspeed);
 
 }
+
+ISR(TIMER3_COMPA_vect){
+  curStepCount++;
+  TCNT3 = 0;  
+  OCR3A = reload * stepSpeed;
+  if(stepSpeed < 4990){
+    digitalWrite(STEP_PIN, HIGH);
+    digitalWrite(STEP_PIN, LOW);
+  }
+  if(curStepCount > targetStepCount){
+    digitalWrite(ENABLE_PIN, HIGH);
+    curStepCount = 0;
+    TIMSK3 |= (0<<OCIE3A);
+  }
+} //Timer 3 counter
+
+void moveStepper(){
+  char* arg = cmd.next();
+  char* garbage = NULL;
+  if(arg != NULL){
+    targetStepCount = (uint32_t) strtol(arg, &garbage,0);
+  }
+  #ifdef DEBUG_SERIAL
+      Serial.print("Stepper count: ");
+      Serial.print(arg);
+      Serial.print(", ");
+      Serial.println(targetStepCount);
+  #endif
+  digitalWrite(ENABLE_PIN, LOW);
+  noInterrupts();
+  TIMSK3 |= (1<<OCIE3A);
+  interrupts();
+}
+
+void setMotorRPM(){
+
+}
+
+void unknownCommand(){
+  Serial.println("Command not found");
+}
+
+
+
 
 uint8_t update_crc8(uint8_t crc, uint8_t crc_seed){
   uint8_t crc_u, i;
